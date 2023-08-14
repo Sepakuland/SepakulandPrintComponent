ed5fd4d6489c6e55e5a2b9ed3c6063635f11d3b1
Mohammad <mohammmadfr78@gmai.com>
2023-08-14 12:19:01 +0330

    add loadLangMsg.

--- dist/loadLangMsg.js
+++ dist/loadLangMsg.js
@@ -5,14 +5,14 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.loadLangMsg = loadLangMsg;
 var _kendoReactIntl = require("@progress/kendo-react-intl");
-var _faIR = _interopRequireDefault(require("../src/fa-IR.json"));
+var _faIR = _interopRequireDefault(require("./src/fa-IR.json"));
 var _caGeneric = _interopRequireDefault(
   require("cldr-dates-full/main/fa/ca-generic.json")
 );
 var _caGregorian = _interopRequireDefault(
   require("cldr-dates-full/main/fa/ca-gregorian.json")
 );
-var _ar = _interopRequireDefault(require("../src/ar.json"));
+var _ar = _interopRequireDefault(require("./src/ar.json"));
 var _caGeneric2 = _interopRequireDefault(
   require("cldr-dates-full/main/ar/ca-generic.json")
 );
