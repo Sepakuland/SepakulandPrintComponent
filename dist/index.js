f9cd6008f59fb0ad7ecdbdfe21a8ad772cc52d2d
Mohammad <mohammmadfr78@gmai.com>
2023-08-15 09:23:47 +0330

    rewrite index.

--- dist/index.js
+++ dist/index.js
@@ -4,6 +4,18 @@ function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" =
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+Object.defineProperty(exports, "PrintGrid", {
+  enumerable: true,
+  get: function get() {
+    return _PrintGrid["default"];
+  }
+});
+Object.defineProperty(exports, "PrintGridEmpty", {
+  enumerable: true,
+  get: function get() {
+    return _PrintGridEmpty["default"];
+  }
+});
 exports["default"] = void 0;
 var _react = _interopRequireWildcard(require("react"));
 require("./default-main-dark.css");
@@ -18,6 +30,8 @@ var _kendoReactGrid = require("@progress/kendo-react-grid");
 var _kendoReactIntl = require("@progress/kendo-react-intl");
 var _getLangDate = require("./getLangDate");
 var _reactRouterDom = require("react-router-dom");
+var _PrintGrid = _interopRequireDefault(require("./PrintGrid"));
+var _PrintGridEmpty = _interopRequireDefault(require("./PrintGridEmpty"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
@@ -31,8 +45,6 @@ function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o =
 function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i]; return arr2; }
 function _iterableToArrayLimit(arr, i) { var _i = null == arr ? null : "undefined" != typeof Symbol && arr[Symbol.iterator] || arr["@@iterator"]; if (null != _i) { var _s, _e, _x, _r, _arr = [], _n = !0, _d = !1; try { if (_x = (_i = _i.call(arr)).next, 0 === i) { if (Object(_i) !== _i) return; _n = !1; } else for (; !(_n = (_s = _x.call(_i)).done) && (_arr.push(_s.value), _arr.length !== i); _n = !0); } catch (err) { _d = !0, _e = err; } finally { try { if (!_n && null != _i["return"] && (_r = _i["return"](), Object(_r) !== _r)) return; } finally { if (_d) throw _e; } } return _arr; } }
 function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
-// import '../../components/RKGrid/style.css'
-
 var Print = function Print(_ref) {
   var printData = _ref.printData,
     columnList = _ref.columnList,
